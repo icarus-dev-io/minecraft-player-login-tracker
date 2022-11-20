246853d6404ef061acf01fb5df35567bce6f8eca
icarus-dev-io <icarus.dev.io@protonmail.com>
2022-11-20 17:04:03 +0100

    init java/maven project

---
+++ src/main/java/org/icarus/minecraft/plugin/PlayerLoginTrackerApplication.java
@@ -0,0 +1,7 @@
+package org.icarus.minecraft.plugin;
+
+public class PlayerLoginTrackerApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/org/icarus/minecraft/plugin/PlayerLoginTrackerApplicationTest.java
@@ -0,0 +1,14 @@
+package org.icarus.minecraft.plugin;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PlayerLoginTrackerApplicationTest {
+
+    @Test
+    void shouldPass() {
+        assertThat(Boolean.TRUE).isTrue();
+    }
+
+}
